34917a6cc94c8129322cbed6573d639e936c7aee
Lane Evans <laneevans96@gmail.com>
2017-01-31 14:53:59 -0600

    1/31/17 Changes in Lab

--- server.js
+++ server.js
@@ -5,12 +5,12 @@
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+
 var http = require('http');
 var fs = require('fs'); // library to access the file system
 var port = 3165;
-
-var chess = fs.readFileSync('images/chess.jpg');
+var imageNames = ['ace.jpg', 'fern.jpg', 'chess.jpg', 'mobile.jpg', 'bubble'];
+var stylesheet = fs.readFileSync('gallery.css');
 
 function serveImage(filename, req, res) {
 	fs.readFile('images/' + filename, function(err, body){
@@ -29,7 +29,24 @@ function serveImage(filename, req, res) {
 
 var server = http.createServer(function(req, res){
 	switch(req.url) {
-		case "/chess":
+    case "/gallery":
+      var gHtml = imageNames.map(function(fileName){
+        return '<image src="' + fileName + '" alt="a fishing ace at work">'
+      }).join('');
+      var html = '<!doctype html>';
+          html += '<head>';
+          html += '  <title>Gallery</title>';
+          html += '  <link href="gallery.css" rel="stylesheet" type="text/css">';
+          html += '</head>';
+          html += '<body>';
+          html += '  <h1>Gallery</h1>';
+          html += gHtml;
+          html += '  <h1>Hello</h1> Time is ' + Date.now();
+          html += '</body>';
+      res.setHeader('Content-Type', 'text/html');
+      res.end(html);
+      break;
+    case "/chess":
 			serveImage('chess.jpg', req, res);
 			break;
 		case "/fern":
@@ -37,6 +54,13 @@ var server = http.createServer(function(req, res){
 		case "/fern.jpeg":
 			serveImage('fern.jpg', req, res);
 			break;
+    case '/ace.jpg':
+      serveImage('ace.jpg', req, res);
+      break;
+    case '/gallery.css':
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
+      break;
 		default:
 			res.statusCode = 404;
 			res.statusMessage = "Not found";
